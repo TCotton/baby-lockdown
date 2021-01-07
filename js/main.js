24035ba73ac40ce8edc893b544832713a99283b7
Andy Walpole <andy.walpole@gmail.com>
2021-01-07 20:46:37 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const isSupportedEffectiveType = !!navigator?.connection?.effectiveType;
+const isSupportedType = !!navigator?.connection?.type;
+const isSupportedSaveData = !!navigator?.connection?.saveData;
+const isSupportedDeviceMemory = !!navigator?.deviceMemory;
+
+const lowfi = ['slow-2g', '2g', '3g']
+let nc;
+let nm;
+let connection;
+
+if (isSupportedEffectiveType) {
+  nc = navigator?.connection
+  if (nc && nc.effectiveType.length > 0) {
+    if (lowfi.indexOf(nc.effectiveType) >= 0) {
+      connection = 'slow'
+    } else {
+      connection = 'fast'
+    }
+  } else {
+    connection = 'slow'
+  }
+}
+
+if (isSupportedType) {
+  nc = navigator?.connection
+  if (nc && nc.type.length > 0) {
+    if (lowfi.indexOf(nc.type) >= 0) {
+      connection = 'slow'
+    } else {
+      connection = 'fast'
+    }
+  } else {
+    connection = 'slow'
+  }
+}
+
+if(isSupportedSaveData) {
+  nc = navigator?.connection;
+  if(nc.saveData) {
+    connection = 'slow';
+  }
+}
+
+if(isSupportedDeviceMemory) {
+  nm = navigator?.deviceMemory;
+  connection = nm < 1 ? 'slow' : 'fast';
+}
+
+const mediaQuery = window.matchMedia('(max-width: 950px)');
+
+if (mediaQuery.matches) {
+  connection = 'slow';
+}
+
+export { connection };
+
---
+++ js/plugins.js
@@ -0,0 +1,93 @@
+// Avoid `console` errors in browsers that lack a console.
+import { connection } from './main';
+(function () {
+  let method;
+  let noop = function () {
+  };
+  let methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  let length = methods.length;
+  let console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+import videojs from 'video.js';
+import 'wavesurfer.js';
+import WaveSurfer from 'videojs-wavesurfer';
+import largeVideo from './../assets/baby-large.mp4';
+import smallVideo from './../assets/baby-smaller.mp4';
+import poster from './../img/eye.jpg';
+const video = connection === 'slow'? smallVideo: largeVideo;
+const options = {
+  controls: true,
+  autoplay: false,
+  loop: false,
+  width: 950,
+  height: 540,
+  inactivityTimeout: 0,
+  bigPlayButton: false,
+  preload: true,
+  poster: poster,
+  plugins: {
+    wavesurfer: {
+      backend: 'MediaElement',
+      displayMilliseconds: false,
+      debug: true,
+      barGap: 10,
+      waveColor: '#fac32d',
+      progressColor: '#cc3215',
+      cursorColor: 'white',
+      barHeight: 1,
+      cursorWidth: 10,
+      barWidth: 10,
+      barRadius: 0,
+      hideScrollbar: true,
+      // put waveform in separate container
+      container: '#waveform',
+      responsive: true,
+      partialRender: false,
+      scrollParent: true,
+      fillParent: true,
+      interact: false
+    }
+  }
+};
+
+// create player
+let player = videojs('myVideo', options, function() {
+  // print version information at startup
+  const msg = 'Using video.js ' + videojs.VERSION +
+    ' with videojs-wavesurfer ' +
+    videojs.getPluginVersion('wavesurfer') +
+    ' and wavesurfer.js ' + WaveSurfer.VERSION;
+  videojs.log(msg);
+
+  // load file
+  player.src({src: video, type: 'video/mp4'});
+});
+
+player.on('waveReady', function() {
+  console.log('waveform: ready!');
+  const loading = document.querySelector('.loading');
+  loading.forEach(n => n.classList.remove('.loading'));
+});
+
+player.on('playbackFinish', function() {
+  console.log('playback finished.');
+});
+
+// error handling
+player.on('error', function(element, error) {
+  console.warn('ERROR:', error);
+});
